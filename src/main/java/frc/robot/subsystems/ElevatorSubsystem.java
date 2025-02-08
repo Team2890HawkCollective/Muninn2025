d2f9cef0c715ecf09122e8e09f3ef3b85ad3d3cb
hawkCollectiveService <HawkCollectiveService@gmail.com>
2025-02-08 11:45:40 -0500

    Applied PID Configurations to the Elevator and Lift Motors - Ryan

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -12,12 +13,14 @@ import com.revrobotics.spark.SparkClosedLoopController;
 public class ElevatorSubsystem extends SubsystemBase
 {
     private static SparkFlex raiseMotor = new SparkFlex(Constants.Elevator.ELEVATOR_MOTOR_ID, MotorType.kBrushless);
-    private static SparkClosedLoopController elevatorPIDController = raiseMotor.getClosedLoopController();
+    private static SparkClosedLoopController elevatorPIDController;
     public static SparkFlexConfig elevatorPIDConfig = new SparkFlexConfig();
 
     public ElevatorSubsystem()
     {        
         elevatorPIDConfig.closedLoop.pid(Constants.Elevator.PID_P, Constants.Elevator.PID_I, Constants.Elevator.PID_D);
+        raiseMotor.configure(elevatorPIDConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        elevatorPIDController = raiseMotor.getClosedLoopController();
     }
 
     public Command goToBaseStageCommand()
@@ -69,11 +72,4 @@ public class ElevatorSubsystem extends SubsystemBase
     {
         elevatorPIDController.setReference(Constants.Elevator.FOURTH_CORAL_STAGE_ENCODER_VALUE, SparkFlex.ControlType.kPosition);
     }
-/* 
-
-    public Command moveElevatorCommand()
-    {
-        return
-    }
-*/
 }
--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -4,21 +4,24 @@ import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig;
 
 public class LiftSubsystem extends SubsystemBase
 {
     private static SparkMax liftMotor = new SparkMax(Constants.Lift.LIFT_MOTOR_ID, MotorType.kBrushless);
-    private static SparkClosedLoopController liftPIDController = liftMotor.getClosedLoopController();
+    private static SparkClosedLoopController liftPIDController;
     private static SparkMaxConfig liftPIDConfig= new SparkMaxConfig();
   
   /** Creates a new AmpSubsystem. */
   public LiftSubsystem() 
   {    
     liftPIDConfig.closedLoop.pid(Constants.Lift.PID_P, Constants.Lift.PID_I, Constants.Lift.PID_D);
+    liftMotor.configure(liftPIDConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    liftPIDController = liftMotor.getClosedLoopController();
   }
 
   public Command goToStartStageCommand()
@@ -39,7 +42,6 @@ public class LiftSubsystem extends SubsystemBase
   public void startStage()
   {
     liftPIDController.setReference(Constants.Lift.START_STAGE_ENCODER_VALUE, SparkMax.ControlType.kPosition);
-
   }
 
   public void catchStage()
@@ -50,6 +52,5 @@ public class LiftSubsystem extends SubsystemBase
   public void liftStage()
   {
     liftPIDController.setReference(Constants.Lift.LIFT_STAGE_ENCODER_VALUE, SparkMax.ControlType.kPosition);
-
   }
 }
