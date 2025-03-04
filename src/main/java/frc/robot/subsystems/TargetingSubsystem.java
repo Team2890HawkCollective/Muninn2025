b998668504120b05210cc66e2e48b9f92524f278
JaWarrior12 <97358009+JaWarrior12@users.noreply.github.com>
2025-03-04 10:11:59 -0500

    Forgot the .getLatestResults() call

--- src/main/java/frc/robot/subsystems/TargetingSubsystem.java
+++ src/main/java/frc/robot/subsystems/TargetingSubsystem.java
@@ -131,7 +131,7 @@ public class TargetingSubsystem extends SubsystemBase {
             LimelightHelpers.PoseEstimate limelightBotPoseEstimateMT = LimelightHelpers
                     .getBotPoseEstimate_wpiBlue_MegaTag2(Constants.LimeLight.LIMELIGHT_NAME
             //LimelightHelpers.LimelightTarget_Fiducial jsonData = new LimelightHelpers.LimelightTarget_Fiducial();
-            LimelightHelpers.LimelightTarget_Fiducial jsonData = new LimelightHelpers.LimelightResults.targets_Fiducials[0]; // It should be one of these two calls
+            LimelightHelpers.LimelightTarget_Fiducial jsonData = new LimelightHelpers.LimelightResults.geLatestResults(Constants.Limelight.LIMELIGHT_NAME).targets_Fiducials[0]; // It should be one of these two calls
             Pose2d estimatedFieldPose = jsonData.getRobotPose_TargetSpace2D();
             m_field.setRobotPose(estimatedFieldPose);
 
