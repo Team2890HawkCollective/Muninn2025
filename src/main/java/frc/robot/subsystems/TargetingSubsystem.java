68636e9a86f07bc71f5cb680b549df4b440d173d
JaWarrior12 <97358009+JaWarrior12@users.noreply.github.com>
2025-03-17 10:55:12 -0400

    Targeting now Uses DeferredCommand
    3/17 - I saw a CD post about someone trying this, and their bot always went the wrong way. Regular commands create the command when they are called in the bindings using whatever values exist at that time. This DeferredCommand means the command will be created when the button is pressed, this means it will use the current values. Since regular is created on startup, it will always take (0,0) as initial pose.

--- src/main/java/frc/robot/subsystems/TargetingSubsystem.java
+++ src/main/java/frc/robot/subsystems/TargetingSubsystem.java
@@ -107,9 +107,9 @@ public class TargetingSubsystem extends SubsystemBase {
     }
 
     public Command autoAlignmentCommand(String location) {
-        // return autoAlignmentPose(location); // This is the pathfindToPose version.
-        //return autoAlignmentOffset(location); // This is based off of the work of teams 910 Foley Freeze and 4915 Spartronics. This generates a path given the bot's current pose and offset tag pose
-        return autoAlignmentBasic();
+        // return new DefferredCommand(()->autoAlignmentPose(location),Set.of(swerveSub)); // This is the pathfindToPose version.
+        //return new DeferredCommand(()->autoAlignmentOffset(location),Set.of(swerveSub)); // This is based off of the work of teams 910 Foley Freeze and 4915 Spartronics. This generates a path given the bot's current pose and offset tag pose
+        return new DeferredCommand(()->autoAlignmentBasic(),Set.of(swerveSub));
     }
 
     public Command pathfindTest() {
