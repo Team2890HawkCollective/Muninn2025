ec2d298147cea34ddd4dfbebb73a189dc1983f77
hawkCollectiveService <HawkCollectiveService@gmail.com>
2025-03-21 19:17:40 -0400

    3/21 Inverted Drive FBLR Controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -66,8 +66,8 @@ public class RobotContainer {
      * by angular velocity.
      */
     SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
-            () -> driverXbox.getLeftY() * 1,
-            () -> driverXbox.getLeftX() * 1)
+            () -> driverXbox.getLeftY() * -1,
+            () -> driverXbox.getLeftX() * -1)
             .withControllerRotationAxis(() -> driverXbox.getRightX() * -1)
             .deadband(OperatorConstants.DEADBAND)
             .scaleTranslation(0.8)
