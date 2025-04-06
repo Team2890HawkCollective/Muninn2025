a314e51e63f2e69870c923651867fd65f07f040d
hawkCollectiveService <HawkCollectiveService@gmail.com>
2025-04-06 09:28:38 -0400

    Comment explanations for ShuffleboardDisplay.java

--- src/main/java/frc/robot/ShuffleboardDisplay.java
+++ src/main/java/frc/robot/ShuffleboardDisplay.java
@@ -30,8 +30,8 @@ public class ShuffleboardDisplay {
 
     //private ShuffleboardTab mainTab = Shuffleboard.getTab("Main"); // Example: Making more tabs pn the dashboard
     //private ShuffleboardTab PID_Tab = Shuffleboard.getTab("PID_Tab");
-    private final SendableChooser<String> universalModeChooser = new SendableChooser<>();
-    private final static SendableChooser<String> controlChooser = new SendableChooser<>();
+    private final SendableChooser<String> universalModeChooser = new SendableChooser<>(); // Dropdown Selection; NOTE: The modeChooser doesn't work because we need autos loaded before we can make a selection.
+    private final static SendableChooser<String> controlChooser = new SendableChooser<>(); // Auto Path Dropdown
         private SendableChooser<Command> autoChooser;
         //private GenericEntry genericEntryTest = mainTab.add("Generic",0).getEntry(); // Example: Putting Data on other tabs
     
@@ -45,15 +45,15 @@ public class ShuffleboardDisplay {
         }
     
     
-        public void initializeAutoChooser(){
+        public void initializeAutoChooser(){ // Puts auto commands into the dropdown. It returns the actual command based on the choice, makes things easy.
             if(ShuffleboardConstants.UNIVERSAL_MODE_CHOICE.equalsIgnoreCase("competition")) {
-                    autoChooser = AutoBuilder.buildAutoChooserWithOptionsModifier((stream) -> true? stream.filter(auto -> auto.getName().startsWith("comp_")): stream);
+                    autoChooser = AutoBuilder.buildAutoChooserWithOptionsModifier((stream) -> true? stream.filter(auto -> auto.getName().startsWith("comp_")): stream); // Display autos who's names contains the specified prefix.
             } else if(ShuffleboardConstants.UNIVERSAL_MODE_CHOICE.equalsIgnoreCase("testing")){
-                    autoChooser = AutoBuilder.buildAutoChooserWithOptionsModifier((stream) -> true? stream.filter(auto -> auto.getName().startsWith("test_")): stream);
+                    autoChooser = AutoBuilder.buildAutoChooserWithOptionsModifier((stream) -> true? stream.filter(auto -> auto.getName().startsWith("test_")): stream); // ^
             } else if(ShuffleboardConstants.UNIVERSAL_MODE_CHOICE.equalsIgnoreCase("allAutos")){
                     autoChooser = AutoBuilder.buildAutoChooser();
             } else { //Default Puts All Commands. This is redundant because all the cases are hard coded.
-                    autoChooser = AutoBuilder.buildAutoChooser();
+                    autoChooser = AutoBuilder.buildAutoChooser(); 
             }
             SmartDashboard.putData("Autonomous Choices", autoChooser);
         }
